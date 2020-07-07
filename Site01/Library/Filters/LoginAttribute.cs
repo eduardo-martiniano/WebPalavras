3a8eaf8406c7fe3cbf7ec6b4b0823cb6aa6a5fd7
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2020-07-06 23:45:28 -0300

    Filtro de login adcionado

---
+++ Site01/Library/Filters/LoginAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Site01.Library.Filters
+{
+    public class LoginAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Session.GetString("Login") == null)
+            {
+                context.Result = new RedirectToActionResult("Login", "Home", null);
+            }
+
+            
+        }
+    }
+}
